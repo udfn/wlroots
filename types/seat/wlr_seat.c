udfn/wlroots

commit 24b39e07277fcc9814f4b798ec3ddd19b9c68ced

Advertise wl_seat version 7
This does not require any code changes, as we already copy the keymap
data separately for each client.

For details, see https://gitlab.freedesktop.org/wayland/wayland/commit/905c0a341ddf0a885811d19e2b79c65a3f1d210c

types/seat/wlr_seat.c
@@ -12,7 +12,7 @@
 #include "types/wlr_seat.h"
 #include "util/signal.h"
 
-#define SEAT_VERSION 6
+#define SEAT_VERSION 7
 
 static void seat_handle_get_pointer(struct wl_client *client,
 		struct wl_resource *seat_resource, uint32_t id) {

commit cc52ecf590158ef9e942277d6120bd2d1865adaa

meson: fix wayland-server minimum required version
Having 1.16 results in the following error when running the compositor:

    2019-04-27 17:30:50 - [wayland] wl_global_create: implemented version for 'wl_seat' higher than interface version (7 > 6)
    2019-04-27 17:30:50 - [sway/input/seat.c:428] seat_create:could not allocate seat

We require wayland-server >= 1.17 for wl_seat version 7.

Fixes: 24b39e07277f ("Advertise wl_seat version 7")
Fixes: ac7c1963d3ea ("seat: fallback to v6 if libwayland 1.17 isn't available")

meson.build
@@ -95,7 +95,7 @@ if cc.get_id() == 'clang'
 	add_project_arguments('-Wno-missing-braces', language: 'c')
 endif
 
-wayland_server = dependency('wayland-server', version: '>=1.16')
+wayland_server = dependency('wayland-server', version: '>=1.17')
 wayland_client = dependency('wayland-client')
 wayland_egl = dependency('wayland-egl')
 wayland_protos = dependency('wayland-protocols', version: '>=1.17')

types/seat/wlr_seat.c
@@ -254,13 +254,8 @@ struct wlr_seat *wlr_seat_create(struct wl_display *display, const char *name) {
 	seat->touch_state.seat = seat;
 	wl_list_init(&seat->touch_state.touch_points);
 
-	// TODO: always use SEAT_VERSION (requires libwayland 1.17)
-	uint32_t version = SEAT_VERSION;
-	if (wl_seat_interface.version < SEAT_VERSION) {
-		version = wl_seat_interface.version;
-	}
 	seat->global = wl_global_create(display, &wl_seat_interface,
-		version, seat, seat_handle_bind);
+		SEAT_VERSION, seat, seat_handle_bind);
 	if (seat->global == NULL) {
 		free(touch_grab);
 		free(pointer_grab);