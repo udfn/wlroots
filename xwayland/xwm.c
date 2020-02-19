udfn/wlroots

commit 29f0f82671a68ce8226dc78b1db3802f8fabcba4

xwm: allow applications to change focus between their own surfaces
Although currently this problem is present in only Steam, and it is
actually a client bug.

xwayland/xwm.c
@@ -1234,8 +1234,17 @@ static void xwm_handle_focus_in(struct wlr_xwm *xwm,
 
 	// Do not let X clients change the focus behind the compositor's
 	// back. Reset the focus to the old one if it changed.
-	if (!xwm->focus_surface || ev->event != xwm->focus_surface->window_id) {
+	//
+	// Note: Some applications rely on being able to change focus, for ex. Steam:
+	// https://github.com/swaywm/sway/issues/1865
+	// Because of that, we allow changing focus between surfaces belonging to the
+	// same application.
+	struct wlr_xwayland_surface *requested_focus = lookup_surface(xwm, ev->event);
+	if (!xwm->focus_surface || !requested_focus ||
+			requested_focus->pid != xwm->focus_surface->pid) {
 		xwm_send_focus_window(xwm, xwm->focus_surface);
+	} else {
+		xwm->focus_surface = requested_focus;
 	}
 }
 

commit 279a69cc0b4450367de928a5ac0ff5d4ad8ffd4b

xwayland: ignore pointer focus changes
This reflects what i3 does [1].

[1]: https://github.com/i3/i3/blob/b3faf9fca9254679a4715486a4de80ebaee70410/src/handlers.c#L1076

Fixes: 29f0f82671a6 ("xwm: allow applications to change focus between their own surfaces")
Closes: https://github.com/swaywm/sway/issues/4926

xwayland/xwm.c
@@ -1231,6 +1231,10 @@ static void xwm_handle_focus_in(struct wlr_xwm *xwm,
 			ev->mode == XCB_NOTIFY_MODE_UNGRAB) {
 		return;
 	}
+	// Ignore pointer focus change events
+	if (ev->detail == XCB_NOTIFY_DETAIL_POINTER) {
+		return;
+	}
 
 	// Do not let X clients change the focus behind the compositor's
 	// back. Reset the focus to the old one if it changed.