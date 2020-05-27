udfn/wlroots

commit 81da0061b4eac1619bda5df1c4c04c9b3b6e3a8d

backend/drm: remove missing cursor plane workaround
We have a workaround for legacy drivers that support drmModeSetCursor
without exposing a cursor plane. It doesn't work anymore now that we've
moved to a more atomic interface.

Let's just remove this workaround and fallback to software cursors.

Closes: https://github.com/swaywm/wlroots/issues/2166

backend/drm/drm.c
@@ -941,14 +941,8 @@ static bool drm_connector_set_cursor(struct wlr_output *output,
 	}
 
 	struct wlr_drm_plane *plane = crtc->cursor;
-	if (!plane) {
-		// We don't have a real cursor plane, so we make a fake one
-		plane = calloc(1, sizeof(*plane));
-		if (!plane) {
-			wlr_log_errno(WLR_ERROR, "Allocation failed");
-			return false;
-		}
-		crtc->cursor = plane;
+	if (plane == NULL) {
+		return false;
 	}
 
 	if (!plane->surf.gbm) {

commit eb456385ffa7add8e0f85bcc8cfe490523be1be3

backend/drm: fix missing crtc->cursor NULL check
Fixes: 81da0061b4ea ("backend/drm: remove missing cursor plane workaround")

backend/drm/drm.c
@@ -977,6 +977,9 @@ static bool drm_connector_move_cursor(struct wlr_output *output,
 		return false;
 	}
 	struct wlr_drm_plane *plane = conn->crtc->cursor;
+	if (!plane) {
+		return false;
+	}
 
 	struct wlr_box box = { .x = x, .y = y };
 